ae29d0bd1fd710ae47d6a93234dfb0654b13732f
tsachkova <tsachkova@gmail.com>
2023-02-23 00:27:07 +0200

    3

--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
-let language = document.querySelector('.language');
-let languageP = language.previousElementSibling;
-let languageSpan = languageP.previousElementSibling;
+function addMenuLenguage() {
+    let language = document.querySelector('.language');
+    let languageP = language.previousElementSibling;
+    let languageSpan = languageP.previousElementSibling;
 
-language.addEventListener("click", (e) => {
-    languageP.classList.toggle('visible');
-})
+    language.addEventListener("click", (e) => {
+        languageP.classList.toggle('visible');
+    })
 
-languageP.addEventListener('click', (e) => {
-    let spanText = languageSpan.innerHTML;
-    languageSpan.innerHTML = languageP.innerHTML;
-    languageP.innerHTML = spanText;
-})
+    languageP.addEventListener('click', (e) => {
+        let spanText = languageSpan.innerHTML;
+        languageSpan.innerHTML = languageP.innerHTML;
+        languageP.innerHTML = spanText;
+    })
+}
+
+addMenuLenguage();
 
 $('.main-slider__body').slick();
 
@@ -37,12 +41,67 @@ $('.fitness-slider__body').slick({
     focusOnSelect: true,
 });
 
-let menuButton = document.querySelector('.menu__button');
-let menu = document.querySelector('.menu');
+function addMenuButton() {
+    let menuButton = document.querySelector('.menu__button');
+    let menu = document.querySelector('.menu');
 
-menuButton.addEventListener('click', e => {
+    menuButton.addEventListener('click', e => {
 
-    if (e.target == menuButton) {
-        menu.classList.toggle('open');
+        if (e.target == menuButton) {
+            menu.classList.toggle('open');
+        }
+    })
+
+    let menuButtonFooter = document.querySelector('.footer__button');
+    let footerMenu = document.querySelector('.footer__item-nav');
+
+    menuButtonFooter.addEventListener('click', e => {
+
+        if (e.target == menuButtonFooter) {
+
+            footerMenu.classList.toggle('footer-menu__open');
+        }
+    })
+}
+
+addMenuButton();
+
+$('.zoom').zoomy();
+
+function toggleLupa() {
+    if (window.innerWidth > 1140) {
+        let slideShow = document.querySelector('.fitness-slider__show');
+
+
+        slideShow.addEventListener('mouseenter', (e) => {
+            document.querySelector('.fitness-slider__lupa').style.display = "none";
+        })
+
+        slideShow.addEventListener('mouseleave', (e) => {
+            if (e.target == slideShow) {
+
+                document.querySelector('.fitness-slider__lupa').style.display = "block";
+            }
+        })
     }
-})
+}
+
+toggleLupa();
+
+function setFitnesImgHeight() {
+
+    let imgWeidth = document.querySelector('.fitness-slider__show-img').offsetHeight;
+    document.querySelector('.img-wrapper').style.height = imgWeidth;
+    console.log(imgWeidth);
+}
+
+setTimeout(setFitnesImgHeight, 500);
+
+window.addEventListener('resize', (e) => {
+    document.addEventListener('mousemove', function handler(e) {
+        location.reload();
+        this.removeEventListener('mousemove', handler)
+
+    })
+
+});
